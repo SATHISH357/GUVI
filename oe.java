c0a27c2866a29243cfe34ddd542e747fbc9cf5f3
SATHISH357 <sathishperiyasamy1997@gmail.com>
2017-04-10 18:46:35 +0530

    Create oe.java

---
+++ oe.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class oe
+{
+public static void main(String a[])
+{
+int a=Integer.valueOf(a[0]);
+
+if(a%2==0)
+
+System.out.println("EVEN");
+
+else
+
+System.out.println("ODD");
+
+}
+}
