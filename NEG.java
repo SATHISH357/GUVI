cfe2a22c7a8cfcc856ad8da403280b5d83525f00
SATHISH357 <sathishperiyasamy1997@gmail.com>
2017-04-10 14:37:19 +0530

    Create NEG.java

---
+++ NEG.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class pn
+{
+public static void main(String args[])
+{
+int n=Integer.parseInt(args[0]);
+if(n>0)
+System.out.println("POSITIVE");
+else if(n<0)
+System.out.println("NEGATIVE");
+else
+System.out.println("ZERO");
+}
+}
