78b3bbad21aa8bd9fcdfdb10c69a6c8929c5502c
SATHISH357 <sathishperiyasamy1997@gmail.com>
2017-04-18 23:17:25 +0530

    Create max.java

---
+++ max.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class max
+{
+public static void main(String args[])
+{
+int a=Integer.valueOf(args[0]);
+int b=Integer.valueOf(args[1]);
+int c=Integer.valueOf(args[2]);
+System.out.println(Math.max(a,Math.max(b,c));
+}
+}
