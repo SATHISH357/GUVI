2e142c74876deb384e88f3070d67f85b2d58caa0
SATHISH357 <sathishperiyasamy1997@gmail.com>
2017-04-18 23:06:34 +0530

    Create vrn.java

---
+++ vrn.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.Scanner;
+class vrn
+{
+public static void main(String ar[])
+{
+char a[5]={'a','e','i','o','u'};
+Scanner s=new Scanner(System.in);
+char c=s.next();
+if(c.equalsIgnoreCase(a[0])||c.equalsIgnoreCase(a[1])||c.equalsIgnoreCase(a[2])||c.equalsIgnoreCase(a[3])||c.equalsIgnoreCase(a[4]))
+System.out.println("Vowel");
+else
+System.out.println("consonant");
+}
+}
