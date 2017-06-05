3b5e13dccafdf3ac79a001e2ced63a88a2bf320f
SATHISH357 <sathishperiyasamy1997@gmail.com>
2017-06-05 15:20:13 +0530

    Create cp.java

---
+++ cp.java
@@ -0,0 +1,27 @@
+ublic class cp{
+ 
+    public static void main(String[] args) {
+        
+  Scanner sc = new Scanner(System.in);
+  System.out.println("Enter the number: ");
+  int input = sc.nextInt();
+  int count = 0;
+  for (int i = 2; i < input; i++)
+ {
+boolean isPrimeNumber = true;
+for (int j=2; j<i;j++)
+ {
+   if (i%j== 0) {
+   isPrimeNumber = false;
+   break; 
+    }
+  }
+  if (isPrimeNumber)
+ {
+   count++;
+   System.out.print(i + ", ");
+}
+}
+  System.out.println("Count: " + count);
+    }
+}
