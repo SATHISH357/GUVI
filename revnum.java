6e95fee73e31366a42a612138ae35f8284940cfe
SATHISH357 <sathishperiyasamy1997@gmail.com>
2017-06-06 14:19:28 +0530

    Create revnum.java

---
+++ revnum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.Scanner;
+class revnum
+{
+public suatic void main(String aa[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+String s=String.valueOf(n);
+StringBuffer ss=new StringBuffer(s);
+}
+}
