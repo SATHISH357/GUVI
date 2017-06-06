28acd91229f3780acff53ce5bbf1870dc566e2c8
SATHISH357 <sathishperiyasamy1997@gmail.com>
2017-06-06 14:45:03 +0530

    Create q.java

---
+++ q.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.Scanner;
+class
+{
+public static void main(String aa[])
+{
+Scanner s=new Scanner(System.in);
+String ss=s.nextLine();
+if(ss.equals("Q"))
+System.exit(0);
+}
+}
