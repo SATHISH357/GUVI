d10d6398314ec136fdf71ef899238bb7645b76d4
SATHISH357 <sathishperiyasamy1997@gmail.com>
2017-06-15 11:31:25 +0530

    Create count1.java

---
+++ count1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Scanner;
+class count1
+{
+public static void main(String a[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+System.out.println(s.length());
+}
+}
