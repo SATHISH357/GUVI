90d8d377b29c6d187ae4a49edfc950a6750e4636
SATHISH357 <sathishperiyasamy1997@gmail.com>
2017-06-30 17:45:10 +0530

    Create npri.java

---
+++ npri.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class npri
+{
+	public static void main (String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int num=s.nextInt();int count=0,k=0;
+        for(int i=1;i<=num;i++) 
+        {
+            count = 0;
+            for (int j=2;j<=i/2;j++) 
+            {
+                if(i%j==0) 
+                {
+                count++;
+                break;
+                }
+            }
+            if(count==0) 
+            {
+                k=k+i;
+            }
+        }
+        System.out.println(k);
+	}
+}
